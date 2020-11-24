b55209ac06095b69d6810d8eff8f0999356f9047
Matej Prerovsky <57071246+Botaniculus@users.noreply.github.com>
2020-11-24 20:25:05 +0100

    Add files via upload

---
+++ stub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+MYSELF=`which "$0" 2>/dev/null`
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+exec "$java" $java_args -jar $MYSELF "$@"
+exit 1 
